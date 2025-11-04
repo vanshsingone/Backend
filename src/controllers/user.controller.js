de217080afec06c62324a45dc29b2fe4f96c6c91
vanshsingone <vanshsingone@gmail.com>
2025-11-04 10:09:03 +0530

    add getWatchHistory function and fix changeCurrentPassword typo in user controller and routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,6 +6,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
 import { appendFile } from "fs";
 import { subscribe } from "diagnostics_channel";
+import mongoose from "mongoose";
 
 const generateAccessAndRefereshTokens = async(userId) => {
         try {
@@ -218,7 +219,7 @@ const refreshAccessToken = asyncHandler(async(req,res) => {
    }
 })
 
-const chnageCurrentPassword = asyncHandler(async(req,res) => {
+const changeCurrentPassword = asyncHandler(async(req,res) => {
     const {oldPassord, newPassword} = req.body
 
     const user = await User.findById(req.user?._id)
@@ -384,16 +385,66 @@ const getUserChannelProfile = asyncHandler(async(req,res) => {
         new ApiResponse(200,channel[0],"channel fetched successfully")
     )
 })
+
+const getWatchHistory = asyncHandler(async(req,res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup:{
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar:1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                               $first: "$owner" 
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res.status(200).json(
+        new ApiResponse(200,user[0].watchHistory,"Watch History Fetched successg=fully")
+    )
+})
+
 export { 
     registerUser , 
     loginUser,
     logoutUser,
     refreshAccessToken,
-    chnageCurrentPassword,
+    changeCurrentPassword,
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 
 }
